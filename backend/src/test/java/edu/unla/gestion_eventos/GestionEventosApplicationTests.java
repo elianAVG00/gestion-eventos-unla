1de5391528280aa1fc5bf0192ffab77236a39090
Elian Ariel Vicente Gonzlez <evgonzalez@provincianet.com.ar>
2025-07-08 21:03:27 -0300

    Primer commit inicialización proye con Spring Boot

---
+++ backend/src/test/java/edu/unla/gestion_eventos/GestionEventosApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.unla.gestion_eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
